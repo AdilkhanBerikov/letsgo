c9b99695348d04719b688334e121273d70d2591b
AdilkhanBerikov <fant333777@gmail.com>
2025-03-23 18:39:53 +0500

    fix: button-transport; delete: button-transport.js

--- src/assets/scripts/button-transport.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const transportButtons = document.querySelectorAll(".button__transport");
-
-  transportButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      button.classList.toggle("-off");
-    });
-  });
-});
